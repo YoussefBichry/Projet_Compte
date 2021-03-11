e08490ba9b2845d9805a88e38c959a096559cf20
YoussefBichry <75500068+YoussefBichry@users.noreply.github.com>
2021-03-11 19:38:12 +0100

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte
+{
+    class Client
+    {
+        private string Nom;
+        private string Prenom;
+        private string Adresse;
+        public Client()
+        {
+            Nom = "";
+            Prenom = "";
+            Adresse = "";
+        }
+        public Client(string a,string b,string c)
+        {
+            Nom = a;
+            Prenom = b;
+            Adresse = c;
+        }
+        public void afficher()
+        {
+            Console.Out.WriteLine("Nom :" + Nom + ".");
+            Console.Out.WriteLine("Prenom :" + Prenom + ".");
+            Console.Out.WriteLine("Adresse :" + Adresse + ".");
+
+        }
+    }
+}
